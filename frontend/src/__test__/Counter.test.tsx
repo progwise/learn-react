a770bf9dfa87c24d3a695e40ced8acf63699d0d6
Pascal Helbig <pascal.helbig@progwise.net>
2023-03-21 14:51:06 +0100

    test: install vitest

---
+++ frontend/src/__test__/Counter.test.tsx
@@ -0,0 +1,9 @@
+import { describe, expect, it } from "vitest";
+
+describe("Counter", () => {
+  it("should render two buttons", () => {
+    const sum = 1 + 2;
+
+    expect(sum).toBe(3);
+  });
+});
