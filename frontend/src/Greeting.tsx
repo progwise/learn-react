b614d1d525f3402a793efca9db50578477fbc519
Pascal Helbig <pascal.helbig@progwise.net>
2023-04-11 14:09:01 +0200

    feat: hide greeting button after greeting back

--- frontend/src/Greeting.tsx
+++ frontend/src/Greeting.tsx
@@ -1,16 +1,25 @@
+import { useState } from "react";
+
 export interface GreetingProps {
   names: string[];
   onGreetBack: () => void;
 }
 
 export const Greeting = (props: GreetingProps) => {
+  const [hasGreetedBack, setHasGreetedBack] = useState(false);
+
+  console.log("hasGreetedBack", hasGreetedBack);
+
   const handleClick = () => {
     props.onGreetBack();
+    setHasGreetedBack(true);
   };
 
   const handleMouseEnter = () => {
     console.log("button was entered");
   };
+
+  const showGreetingButton = !hasGreetedBack;
 
   return (
     <div>
@@ -20,9 +29,17 @@ export const Greeting = (props: GreetingProps) => {
           return <li key={name}>{name}</li>;
         })}
       </ul>
-      <button onClick={handleClick} onMouseEnter={handleMouseEnter}>
-        Greet back
-      </button>
+
+      {showGreetingButton && (
+        <button onClick={handleClick} onMouseEnter={handleMouseEnter}>
+          Greet back
+        </button>
+      )}
+      {false}
+      {undefined}
+      {null}
+      {true}
+      {hasGreetedBack ? "Has greeted back" : "has not greeted back"}
     </div>
   );
 };
