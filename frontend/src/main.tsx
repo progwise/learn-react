acf79b2ae683631013b36e11556307c1835fe5fc
Pascal Helbig <pascal.helbig@progwise.net>
2023-04-11 16:43:54 +0200

    feat: add router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { Counter } from "./Counter";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <div style={{ display: "flex", gap: "8px" }}>
+        <Link to="/playground">Playground</Link>
+        <Link to="/counter">Counter</Link>
+      </div>
+      <Routes>
+        <Route path="/playground" element={<App />} />
+        <Route
+          path="/counter"
+          element={<Counter start={0} min={0} max={10} />}
+        />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
