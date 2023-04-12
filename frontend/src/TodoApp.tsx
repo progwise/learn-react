d9b3002f7cf553bcdd641c50aae1b77b1f790d51
Pascal Helbig <pascal.helbig@progwise.net>
2023-04-12 15:56:07 +0200

    feat: add priority to create form

--- frontend/src/TodoApp.tsx
+++ frontend/src/TodoApp.tsx
@@ -3,12 +3,20 @@ import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
+enum Priority {
+  HIGH = "high",
+  MEDIUM = "medium",
+  LOW = "low",
+}
+
 interface CreateTodoForm {
   title: string;
+  priority: Priority;
 }
 
 const CreateTodoSchema = z.object({
   title: z.string().trim().min(1, "mindestens 1 Zeichen").max(100),
+  priority: z.nativeEnum(Priority),
 });
 
 export const TodoApp = () => {
@@ -34,6 +42,12 @@ export const TodoApp = () => {
           disabled={isSubmitting}
         />{" "}
         {errors.title?.message}{" "}
+        <select {...register("priority")} disabled={isSubmitting}>
+          <option value={Priority.HIGH}>High</option>
+          <option value={Priority.MEDIUM}>Medium</option>
+          <option value={Priority.LOW}>Low</option>
+        </select>{" "}
+        {errors.priority?.message}{" "}
         <button type="submit" disabled={isSubmitting}>
           Create
         </button>
