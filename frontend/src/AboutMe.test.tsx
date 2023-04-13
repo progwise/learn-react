dab4f18d0cfacefda2bda440a60dbb7bff4e698c
Pascal Helbig <pascal.helbig@progwise.net>
2023-04-13 11:57:41 +0200

    test: install vitest

---
+++ frontend/src/AboutMe.test.tsx
@@ -0,0 +1,11 @@
+describe("AboutMe", () => {
+  test("1 + 2 should be 3", () => {
+    const result = 1 + 2;
+    expect(result).toBe(3);
+  });
+
+  it("should return 3 for 1 + 2", () => {
+    const result = 1 + 2;
+    expect(result).toBe(3);
+  });
+});
